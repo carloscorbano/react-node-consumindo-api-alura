e0b4193279fe4f4177a731416af6bd3e97168109
Carlos Roberto Corbano da Silva <carlos.corbano@hotmail.com>
2023-06-27 11:15:25 -0300

    removido arquivos e adicionado os projetos como submódulos

--- src/componentes/Titulo/index.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Titulo = styled.h2`
-    width: 100%;
-    text-align: ${ props => props.alinhamento || 'center' };
-    color: ${ props => props.cor || '#000' };
-    font-size: ${ props => props.tamanhoFonte || '1rem' };
-`;
--- src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
+++
@@ -1,9 +0,0 @@
-import livroCSS from '../../imagens/CSS-Eficiente.png';
-import livroGuiaFrontEnd from '../../imagens/Guia-front-end.png';
-import livroORetornoDoCangaceiro from '../../imagens/O-Retorno-do-cangaceiro-JavaScript.png';
-
-export const livros = [
-    { nome: 'CSS Eficiente', id: 5, src: livroCSS },
-    { nome: 'Guia Front End', id: 6, src: livroGuiaFrontEnd },
-    { nome: 'O Retorno do Cangaceiro Javascript', id: 7, src: livroORetornoDoCangaceiro }
-]
